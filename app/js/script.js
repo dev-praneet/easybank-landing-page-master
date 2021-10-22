2b88fccfdeb3fc6f8fc7a308c5a78c3dd3ac822a
geeky-amat <geeky.amat@gmail.com>
2021-10-22 08:41:42 +0530

    initial commit

---
+++ app/js/script.js
@@ -0,0 +1,29 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+    
+    if(header.classList.contains('open')){ //Close hamburger menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');            
+        });
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else{ //Open hamburger menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+
+    }
+});
